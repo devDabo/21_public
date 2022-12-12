c2035e27b402b6d586e54365c336283e3b4b5b9e
c1673142 <UNORev31410>
2022-12-12 12:54:09 +0000

    amended logic

--- main.py
+++ main.py
@@ -1,154 +1,77 @@
 import random
 
+#Defined three empty lists. They have to be filled with zeros as the cards are not appended to them.
+card_split_one = [0,0,0,0,0,0,0]
+card_split_two = [0,0,0,0,0,0,0]
+card_split_three = [0,0,0,0,0,0,0]
+
+#The 21 cards are added to card_orig, which are then added to card_list
+card_orig = []
 card_list = []
+
+card_list.append(card_orig)
 
 suit = ["Diamonds" , "Clubs", "Hearts", "Spades"]
 card_value = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
 
-pack=[[0]*52 for x in range(2)]
+# for loop which adds the card value and the suit. % is used to use all the suits for all the pictures
+
 for card_number in range (0,52):
-    pack[0][card_number]=suit[card_number%4]
-    pack[1][card_number]=card_value[card_number%13]
+    card_orig.append(card_value[card_number%13] + " of " + suit[card_number%4])
 
-for i in range(0,4):
-    for j in range(0,13):
-        card_picture = (pack[1][j], "of " + pack[0][i])
-        card_list.append(card_picture)
+random.shuffle(card_orig)
 
-#print(card_list)
-card_split = ([0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0])
+#adds 21 cards from the 52 to card_list.
+for card_number in range (0,21):
+    card_list.append(card_orig[card_number])
 
-# for i in card_split:
-#     for x in i:
-#         print(x)
-#     print(i)
+#Carries out card split and card selection then card_list clear.
+def card_trick():
+    pack_position = 0
 
-random.shuffle(card_list)
+    #using global statement avoid an unbound Local error
+    global card_split_one
+    global card_split_two
+    global card_split_three
 
-pack_position=0
+    #Writes the values from the 21x1 to 7x3, instead of 1,2,3,4 etc. it writes 1,4,7,10 etc.
+    for col in range (0,7):
+        card_split_one[col]=card_list[0][pack_position]
+        pack_position=pack_position+1
+        card_split_two[col]=card_list[0][pack_position]
+        pack_position=pack_position+1
+        card_split_three[col]=card_list[0][pack_position]
+        pack_position=pack_position+1
 
-for row in range (0,7):
-    card_split[0][row]=card_list[pack_position]
-    pack_position=pack_position+1
-    card_split[1][row]=card_list[pack_position]
-    pack_position=pack_position+1
-    card_split[2][row]=card_list[pack_position]
-    pack_position=pack_position+1
-print(card_split)
+    print("Pile 1" , card_split_one)
+    print("Pile 2" , card_split_two)
+    print("Pile 3" , card_split_three)
 
-pack_position=0
+    #if 1st column is picked then the first list is placed in the middle
+    print("Choose a card, tell me which pile it is in")
+    user_input = input()
+    if user_input == "1":
+        print("You chose pile 1")
+        card_split_one,card_split_two=card_split_two, card_split_one
+    elif user_input == "2":
+        print("You chose pile 2")
+    elif user_input == "3":
+        print("You chose pile 3")
+        card_split_three,card_split_two=card_split_two, card_split_three
+    else:
+        print("Invalid input")
 
-#def select(user_input):
-print("Choose a card, tell me which pile it is in")
-user_input = input()
-if user_input == "1":
-    print("You chose Pile 1")
-    pack_position=0
-    for row in range (0,7):
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-elif user_input == "2":
-        print("You chose Pile 2")
-        pack_position=0
-        for row in range (0,7):
-            card_split[2][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[1][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[0][row]=card_list[pack_position]
-            pack_position=pack_position+1
-elif user_input == "3":
-    print("You chose the Pile 3")
-    pack_position=0
-    for row in range (0,7):
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-else:
-    print("Invalid input")
+    #This adds the three lists and combines then into one.
+    combined_array = card_split_one + card_split_two + card_split_three
+    #empties the previous card_list and then adds the new cards to it.
+    del card_list[:]
+    card_list.append(combined_array)
 
-print(card_split)
+#tries is the iterating variable
+for tries in range(0,3):
+    card_trick()
+#iterates and adds to the loop.
+tries = tries + 1
 
-print("Choose a card, tell me which pile it is in")
-user_input = input()
-if user_input == "1":
-    print("You chose Pile 1")
-    pack_position=0
-    for row in range (0,7):
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-elif user_input == "2":
-        print("You chose Pile 2")
-        pack_position=0
-        for row in range (0,7):
-            card_split[2][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[1][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[0][row]=card_list[pack_position]
-            pack_position=pack_position+1
-elif user_input == "3":
-    print("You chose the Pile 3")
-    pack_position=0
-    for row in range (0,7):
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-else:
-    print("Invalid input")
-
-
-print(card_split)
-
-print("Choose a card, tell me which pile it is in")
-user_input = input()
-if user_input == "1":
-    print("You chose Pile 1")
-    pack_position=0
-    for row in range (0,7):
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-elif user_input == "2":
-        print("You chose Pile 2")
-        pack_position=0
-        for row in range (0,7):
-            card_split[2][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[1][row]=card_list[pack_position]
-            pack_position=pack_position+1
-            card_split[0][row]=card_list[pack_position]
-            pack_position=pack_position+1
-elif user_input == "3":
-    print("You chose the Pile 3")
-    pack_position=0
-    for row in range (0,7):
-        card_split[0][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[2][row]=card_list[pack_position]
-        pack_position=pack_position+1
-        card_split[1][row]=card_list[pack_position]
-        pack_position=pack_position+1
-else:
-    print("Invalid input")
-#print(card_split.extend([0]))
-
-print(card_split)
-exit = input("press enter to close")
+#The card will be the 11th card. This will be [10] as the list starts at 0.
+print("Your card is the" , card_list[0][10])
